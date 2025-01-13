1bb403e9b04e89961ae1c11a9df3b4ac6c249977
sahilkhude117 <sahilkhude11@gmail.com>
2025-01-13 14:02:58 +0530

    init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,23 @@
 require("dotenv").config();
 
-import Anthropic from "@anthropic-ai/sdk";
+import { Anthropic } from "@anthropic-ai/sdk";
+import { log } from "console";
 
-const anthropic = new Anthropic();
+const anthropic = new Anthropic({
+    apiKey: process.env.ANTHROPIC_API_KEY,
+});
 
 async function main() {
-    anthropic.messages.stream({
-        messages: [],
+    const msg = await anthropic.messages.create({
         model: "claude-3-5-sonnet-20241022",
-        max_tokens: 1024,
-    }).on('text',(text) => {
-        console.log(text);
+        max_tokens: 1000,
+        temperature: 0,
+        messages: [{
+            role: "user",
+            content: "What is 2 + 2?"
+        }]
     })
+    console.log(msg);
 }
 
 main();
