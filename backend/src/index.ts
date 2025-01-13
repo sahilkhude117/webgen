2b1a7c574884812f985bb58bf8d5363e26a6483f
sahilkhude117 <sahilkhude11@gmail.com>
2025-01-13 15:05:54 +0530

    getSystemPrompt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,24 @@
 require("dotenv").config();
 
 import { Anthropic } from "@anthropic-ai/sdk";
-import { log } from "console";
+import { getSystemPrompt } from "./prompts";
 
 const anthropic = new Anthropic({
     apiKey: process.env.ANTHROPIC_API_KEY,
 });
 
 async function main() {
-    const msg = await anthropic.messages.create({
-        model: "claude-3-5-sonnet-20241022",
-        max_tokens: 1000,
-        temperature: 0,
+    anthropic.messages.stream({
         messages: [{
             role: "user",
-            content: "What is 2 + 2?"
-        }]
-    })
-    console.log(msg);
+            content: "Create a simple web app that displays a list of users and their details."
+        }],
+        model: "claude-3-5-sonnet-20241022",
+        max_tokens: 1000,
+        system: getSystemPrompt(),
+    }).on('text', (text) => {
+        console.log(text);
+    });
 }
 
 main();
